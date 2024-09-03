9d3f54db14e8c483a59e4bd42bb965b1a1ad1d27
Sirinemn <sirinemn@outlook.fr>
2024-09-03 15:26:35 +0200

    add app.config file

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,45 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { JwtInterceptor } from './app/interceptors/jwt.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-
-const materialModule = [
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatSnackBarModule,
-  MatToolbarModule,
-  MatSidenavModule, 
-  MatSelectModule
-];
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app.config';
 
 
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom( BrowserModule, AppRoutingModule, FormsModule, ...materialModule),
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
