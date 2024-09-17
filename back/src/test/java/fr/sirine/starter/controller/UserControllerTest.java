14bf3f9509aff5dcffc3af6e151e860d5dd8a62d
Sirinemn <sirinemn@outlook.fr>
2024-09-17 18:52:33 +0200

    update all tests

--- back/src/test/java/fr/sirine/starter/controller/UserControllerTest.java
+++ back/src/test/java/fr/sirine/starter/controller/UserControllerTest.java
@@ -15,7 +15,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
