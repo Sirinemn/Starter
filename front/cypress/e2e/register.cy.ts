b385c90ffd9a558f6c4a39501a6277bf9952a925
Sirinemn <sirinemn@outlook.fr>
2024-09-22 21:24:19 +0200

    add e2e tests

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -20,5 +20,23 @@ describe('Register spec', () =>{
         cy.get('input[formControlName=email]').type(`${'test@mail.fr'}{enter}{enter}`);
 
         cy.url().should('include', '/activate-account');
+
+        cy.pause();
     });
+    it('Acticate account', () => {
+        cy.visit('/auth/activate-account');
+
+        cy.get('code-input').within(() => {
+            cy.get('input').eq(0).type('1');
+            cy.get('input').eq(1).type('2');
+            cy.get('input').eq(2).type('3');
+            cy.get('input').eq(3).type('4');
+            cy.get('input').eq(4).type('5');
+            cy.get('input').eq(5).type('6');
+          });
+        cy.get('button').contains('Go to Login').click()
+
+        cy.url().should('include', '/auth/login');
+    })
+
 });
