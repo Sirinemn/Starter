b499c690a8c7f12be787c50d305e78705cca5216
Sirinemn <sirinemn@outlook.fr>
2025-01-07 21:05:03 +0100

    update nor-found-component

--- front/src/app/pages/not-found/not-found.component.spec.ts
+++ front/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NotFoundComponent } from './not-found.component';
+import { provideRouter } from '@angular/router';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,7 +8,10 @@ describe('NotFoundComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [NotFoundComponent]
+    imports: [NotFoundComponent],
+    providers: [
+      provideRouter([])
+    ]
 });
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
